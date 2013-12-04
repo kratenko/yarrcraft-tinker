f595d8a1fff0d4e95a9880ca2962479ae975b3fb
kratenko <kratenko@garstig.org>
2013-12-04 04:16:39 +0100

    ds

--- cell.lua
+++ cell.lua
@@ -1,23 +1,25 @@
 function find_cells()
 	local cells = {}
-	local attached = peripherals.getNames()
-	for n, dir in attached do
+	local attached = peripheral.getNames()
+	for n, dir in pairs(attached) do
 		local typ = peripheral.getType(dir)
 		if typ == "redstone_energy_cell" then
-			tables.insert(cells, peripheral.wrap(dir))
+			table.insert(cells, peripheral.wrap(dir))
 		end
 	end
 	return cells
+end
 
 function find_modem()
-	local attached = peripherals.getNames()
-	for n, dir in attached do
+	local attached = peripheral.getNames()
+	for n, dir in pairs(attached) do
 		local typ = peripheral.getType(dir)
 		if typ == "modem" then
 			return peripheral.wrap(dir)
 		end
 	end
 	return nil
+end
 
 local id = os.getComputerID()
 local modem = find_modem()
@@ -25,13 +27,23 @@ local channel = 7
 
 while true do
 	local cells = find_cells()
-	local msg = "cells:"..id
-	for n, cell in cells do
+--	local msg = "cells:"..id
+	local total_cap = 0
+	local total_load = 0
+	local cnt = 0
+	for n, cell in pairs(cells) do
 		local max = cell.getMaxEnergyStored()
 		local cur = cell.getEnergyStored()
-		msg = msg .. ":" .. cur .. "/" max
-	print(msg)
+		cnt = cnt + 1
+		total_cap = total_cap + max
+		total_load = total_load + cur
+--		msg = msg .. ":" .. cur .. "/" .. max
+	end
+--	print(msg)
+	local msg = "cell:"..id..":"..cnt..":"..total_load..":"..total_cap
+	modem.transmit(channel, 0, msg)
 	sleep(3)
+end
 
 
 
